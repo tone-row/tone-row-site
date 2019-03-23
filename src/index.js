8ba8f0be6cb676eeef82a3249ee0a723e131c2e8
Rob Gordon <rchristopher.gordon@gmail.com>
2019-03-23 14:46:43 -0400

    updated font and display

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import 'normalize.css'
-import './assets/sporting-grotesque/font.css'
 import './style.scss'
-
-const solutions = document.getElementById('solutions')
-
-solutions.addEventListener('mouseover', () => {
-  document.body.classList.add('hovering')
-})
-
-solutions.addEventListener('mouseleave', () => {
-  document.body.classList.remove('hovering')
-})
